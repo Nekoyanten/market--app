7c2f6695ba8d1e405875b2f9b58df86cfa07e0bf
Nicolas Y <nyantensantacruz@gmail.com>
2025-09-02 15:38:15 -0500

    agregamos table_usuers

---
+++ Data base/table_usuers.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users (
+	id BIGSERIAL PRIMARY KEY,
+	firstname VARCHAR (30) NOT NULL,
+	lastname VARCHAR (30) NOT NULL,
+	mobile_number VARCHAR (20) NOT NULL,
+	ide_number VARCHAR (15) NOT NULL UNIQUE,
+	address TEXT NOT NULL,
+	birthdate DATE NULL,
+	email VARCHAR (200) NOT NULL UNIQUE,
+	password TEXT NOT NULL,
+	status BOOLEAN DEFAULT TRUE,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+	deleted_at TIMESTAMPTZ NULL
+);
+
+INSERT INTO users(firstname, lastname, mobile_number, ide_number,address, email, password )
+VALUES ('Neko','Yanten','305126452', '1163036631','calle #22','Neko@qwerty.com','123456');
