514d04bc360c3e784b6b7ac7092bbdcf79d926fa
Nicolas Y <nyantensantacruz@gmail.com>
2025-09-04 11:16:37 -0500

    Data base conection

---
+++ config/Database/database.php
@@ -0,0 +1,25 @@
+<?php
+//Database connection
+    $host ="127.0.0.1";
+    $user = "postgres";
+    $dbname = "marketapp";
+    $password = "unicesmag";
+    $port = "5432";
+
+    $data_connection ="
+        host=$host
+        user=$user
+        password =$password
+        dbname=$dbname
+        port=$port
+    ";
+
+    $conn = pg_connect($data_connection);
+
+    if (!$conn){
+        echo "Error";
+    }else{
+        echo" Connection successfully:::";
+    }
+
+?>
