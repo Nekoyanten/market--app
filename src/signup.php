c6ca3d78e3622f7e96dbd5abc2a35836383e245b
Nicolas Y <nyantensantacruz@gmail.com>
2025-09-16 15:33:27 -0500

    Conectar registerphp  con register html

--- src/signup.php
+++ src/signup.php
@@ -13,8 +13,23 @@ $p_wd =$_POST['passwo'];
 
 $enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
 //step 3 query to inset  into 
+$check_email= "
+        SELECT 
+            u.email
+        from
+            users u
+        where
+            email ='$e_mail' or id_numbe='idnumber'
+            LIMIT 1
+        ";
 
-$query="
+$res_check=pg_querry($conn,$check_email);
+if(pg_num_rows($res_check)>0){
+    echo "<script>alert('User already exists!!')</script>";
+    header('refresh:0;url=signup.html')
+
+} else {
+    $query="
     INSERT INTO users(
         firstname,
         lastname ,
@@ -39,10 +54,12 @@ $res=pg_query($conn,$query);
 //step 5  validate result 
 
 if ($res){
-        echo "User has  been create successfully !!!";
+        //echo "User has  been create successfully !!!";
+        echo "<script>alert('Success !!! Go to login')</script>";+
+        header('refresh:0;url=signin.html');
     } else {
             echo "Something wrong!";
         };
-
+}
 
 ?>
